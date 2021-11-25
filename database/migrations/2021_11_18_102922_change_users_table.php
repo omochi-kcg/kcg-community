da5364badf402e87e4b4af5e5eeb2638d0b7db6f
Stefan Larsson <Stefan.per.larsson@gmail.com>
2021-11-25 13:40:30 +0900

    #20 fix down()/migrate:refresh issue

--- database/migrations/2021_11_18_102922_change_users_table.php
+++ database/migrations/2021_11_18_102922_change_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -27,6 +28,13 @@ class ChangeUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+
+            //Since we are setting data to not allow NULL anymore, we need to make sure there are no NULL values.
+            User::whereNull('grade')->orWhereNull('department')->update([
+                'grade' => 0,
+                'department' => '',
+            ]);
+
             $table->integer('grade')->nullable(false)->change();
             $table->string('department')->nullable(false)->change();
         });
