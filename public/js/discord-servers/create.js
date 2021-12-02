c75be60cda9db7b519060a5c787feced78a3e971
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-02 16:43:07 +0900

    ✨ Update add tags button

--- public/js/discord-servers/create.js
+++ public/js/discord-servers/create.js
@@ -1,17 +1,20 @@
 "use strict";
 {
     const addTagButton = document.getElementById("add-button");
+    let count = 0;
 
     addTagButton.addEventListener("click", () => {
         const input = document.createElement("input");
-
         const button = document.createElement("button");
-
+        input.name = `tags[]`;
         input.type = "text";
         input.className =
             "block w-2/3 my-1 placeholder-gray-300 border-gray-300 rounded-md shadow-sm md:w-1/2 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50";
         const tagLabel = document.getElementById("tag");
-        console.log(tagLabel);
         tagLabel.appendChild(input);
+        count++;
+        if (count >= 4) {
+            addTagButton.remove();
+        }
     });
 }
