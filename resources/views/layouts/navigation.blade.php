cebf5e8707f714fc975ff598c66e367fcf6def92
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-02 10:30:21 +0900

    💄 Update navigation. #30

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,89 +2,74 @@
     <!-- Primary Navigation Menu -->
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+            <div class="flex justify-between w-full">
                 <!-- Logo -->
-                <div class="flex items-center flex-shrink-0">
-                    <a href="{{ route('homes.top') }}">
-                        <x-application-logo class="block w-auto h-10 text-gray-600 fill-current" />
-                    </a>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <div class="flex items-center flex-shrink-0">
+                        <a href="{{ route('homes.top') }}">
+                            <x-application-logo class="block w-auto h-10 text-gray-600 fill-current" />
+                        </a>
+                    </div>
+                    <x-nav-link :href="route('discord-servers.index')"
+                        :active="request()->routeIs('discord-servers.index')">
+                        Discordサーバ
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        掲示板
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        授業評価
+                    </x-nav-link>
                 </div>
-
-                <div class="hidden sm:flex sm:items-center sm:ml-6">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button
-                                class="flex items-center text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">
-                                <h2 class="text-lg">contents</h2>
-
-                                <div class="ml-1">
-                                    <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <x-dropdown-link :href="route('discord-servers.index')">
-                                Discordサーバ
-                            </x-dropdown-link>
-                            <x-dropdown-link href="#">
-                                掲示板
-                            </x-dropdown-link>
-                            <x-dropdown-link href="#">
-                                授業評価
-                            </x-dropdown-link>
-                        </x-slot>
-                    </x-dropdown>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('homes.about')" :active="request()->routeIs('homes.about')">
+                        About
+                    </x-nav-link>
+                    @if (Auth::check())
+                        <x-nav-link :href="route('logout')" :active="request()->routeIs('logout')">
+                            Logout
+                        </x-nav-link>
+                    @else
+                        <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                            Login
+                        </x-nav-link>
+                    @endif
+                </div>
+                <!-- Hamburger -->
+                <div class="flex items-center -mr-2 sm:hidden">
+                    <button @click="open = ! open"
+                        class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
+                        <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
+                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
                 </div>
             </div>
-            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+        </div>
+
+        <!-- Responsive Navigation Menu -->
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link :href="route('discord-servers.index')" :active="route('discord-servers.index')">
+                    Discordサーバ
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    掲示板
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    授業評価
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('homes.about')" :active="request()->routeIs('homes.about')">
                     About
-                </x-nav-link>
-                <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
                     Login
-                </x-nav-link>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="flex items-center -mr-2 sm:hidden">
-                <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
-                    <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+                </x-responsive-nav-link>
             </div>
         </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('discord-servers.index')" :active="route('discord-servers.index')">
-                Discordサーバ
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                掲示板
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                授業評価
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                About
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
-                Login
-            </x-responsive-nav-link>
-        </div>
-    </div>
 </nav>
