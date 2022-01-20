01cbc31bdf024d9831f64976800d0d186b3e4496
furukawa <y_ff03242@yahoo.co.jp>
2022-01-20 10:39:47 +0900

    ⚰️ #64 Remove title-font

--- resources/views/discord-servers/about.blade.php
+++ resources/views/discord-servers/about.blade.php
@@ -1,17 +1,17 @@
 <x-app-layout>
   <section class="text-gray-600 body-font">
     <div class="container px-5 pt-20 mx-auto">
-      <h1 class="title-font text-2xl font-bold text-gray-700 mb-3">Discord-無期限招待リンクの作成方法</h1>
+      <h1 class="text-2xl font-bold text-gray-700 mb-3">Discord-無期限招待リンクの作成方法</h1>
     </div>
     <div class="container px-5 py-10 mx-auto">
-      <h1 class="title-font text-xl font-medium text-gray-700 mb-3">①PCから作成</h1>
+      <h1 class="text-xl font-medium text-gray-700 mb-3">①PCから作成</h1>
       <div class="flex flex-wrap -m-4">
         <div class="p-4 md:w-1/3">
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-cover object-center" src="{{ asset('images/pc01.png') }}" alt="pc">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">①</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">招待リンクを作成したいサーバーに入り、「招待を作成」をクリック</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">①</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">招待リンクを作成したいサーバーに入り、「招待を作成」をクリック</h2>
             </div>
           </div>
         </div>
@@ -19,8 +19,8 @@
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-cover object-center" src="{{ asset('images/pc02.png') }}" alt="pc">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">②</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">右下にある「招待リンクを編集する」をクリック</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">②</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">右下にある「招待リンクを編集する」をクリック</h2>
             </div>
           </div>
         </div>
@@ -28,22 +28,22 @@
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-cover object-center" src="{{ asset('images/pc03.png') }}" alt="pc">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">③</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">期限の欄を「期限なし」に変更し、リンクを作成</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">③</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">期限の欄を「期限なし」に変更し、リンクを作成</h2>
             </div>
           </div>
         </div>
       </div>
     </div>
     <div class="container px-5 py-10 mx-auto">
-      <h1 class="title-font text-xl font-medium text-gray-700 mb-3">②Androidから作成</h1>
+      <h1 class="text-xl font-medium text-gray-700 mb-3">②Androidから作成</h1>
       <div class="flex flex-wrap -m-4">
         <div class="p-4 md:w-1/3">
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-contain object-center" src="{{ asset('images/android01.png') }}" alt="android">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">①</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">招待リンクを作成したいサーバーに入り、「メンバーを招待する」をタップ</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">①</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">招待リンクを作成したいサーバーに入り、「メンバーを招待する」をタップ</h2>
             </div>
           </div>
         </div>
@@ -51,22 +51,22 @@
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-cover object-center" src="{{ asset('images/android02.png') }}" alt="android">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">②</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">「このリンクを無期限に設定する」をONにし、リンクを共有</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">②</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">「このリンクを無期限に設定する」をONにし、リンクを共有</h2>
             </div>
           </div>
         </div>
       </div>
     </div>
     <div class="container px-5 py-10 mx-auto">
-      <h1 class="title-font text-xl font-medium text-gray-700 mb-3">③iPhone/iPadから作成</h1>
+      <h1 class="text-xl font-medium text-gray-700 mb-3">③iPhone/iPadから作成</h1>
       <div class="flex flex-wrap -m-4">
         <div class="p-4 md:w-1/3">
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-contain object-center" src="{{ asset('images/iphone01.png') }}" alt="iphone">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">①</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">招待リンクを作成したいサーバーに入り、「メンバーを招待する」をタップ</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">①</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">招待リンクを作成したいサーバーに入り、「メンバーを招待する」をタップ</h2>
             </div>
           </div>
         </div>
@@ -74,8 +74,8 @@
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-contain object-center" src="{{ asset('images/iphone02.png') }}" alt="iphone">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">②</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">右下にある「招待リンクを編集する」をタップ</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">②</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">右下にある「招待リンクを編集する」をタップ</h2>
             </div>
           </div>
         </div>
@@ -83,8 +83,8 @@
           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
             <img class="lg:h-48 md:h-36 w-auto m-auto object-contain object-center" src="{{ asset('images/iphone03.png') }}" alt="iphone">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">③</h2>
-              <h2 class="title-font text-base font-medium text-gray-600 mb-3">詳細設定から「期限なし」を選択し、保存してからリンクを作成</h2>
+              <h2 class="tracking-widest text-xs font-medium text-gray-400 mb-1">③</h2>
+              <h2 class="text-base font-medium text-gray-600 mb-3">詳細設定から「期限なし」を選択し、保存してからリンクを作成</h2>
             </div>
           </div>
         </div>
