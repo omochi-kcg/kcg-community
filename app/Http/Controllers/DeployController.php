15abe38ffcfdc9e7764699e313ef377b41191adf
Stefan Larsson <Stefan.per.larsson@gmail.com>
2022-01-23 21:22:34 +0900

    #70 simpliying the deploy

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -9,20 +9,13 @@ class DeployController extends Controller
 {
     public function index(Request $request)
     {
-
-        $signature = hash_hmac('sha1', file_get_contents('php://input'), config('deploy.secret_key'));
-        $required_headers = array(
-            'REQUEST_METHOD' => 'POST',
-            'HTTP_X_GITHUB_EVENT' => 'push',
-            'HTTP_USER_AGENT' => 'GitHub-Hookshot/*',
-            'HTTP_X_HUB_SIGNATURE' => 'sha1=' . $signature,
-        );
-        var_dump($required_headers);
+        if(!$request->get('key'))
+            return abort(404);
 
         /**
          * Main Section: No need to modify below this line
          */
-        if ($_GET['key'] === config('deploy.secret_key'))  {
+        if ($request->get('key') === config('deploy.secret_key'))  {
             $deploy = new Deploy(config('deploy.options'));
             $deploy->execute();
             /*
