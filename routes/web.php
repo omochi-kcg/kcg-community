f407b3cd2bfcdd8c05d68aefa0e594139a7098f8
Stefan Larsson <Stefan.per.larsson@gmail.com>
2022-01-26 19:47:55 +0900

    fixed board routes

--- routes/web.php
+++ routes/web.php
@@ -24,39 +24,41 @@ Route::middleware(['web'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['auth'])->name('dashboard');
-});
 
-Route::get('/board', [BoardController::class, 'board'])
+    Route::get('/board', [BoardController::class, 'board'])
     ->name('boards.board');
 
-Route::get('/boards/{board}', [BoardController::class, 'show'])
-    ->name('boards.show')
-    ->where('board', '[0-9]+');
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/boards/{board}', [BoardController::class, 'show'])
+            ->name('boards.show')
+            ->where('board', '[0-9]+');
 
-Route::get('/boards/create', [BoardController::class, 'create'])
-    ->name('boards.create');
+        Route::get('/boards/create', [BoardController::class, 'create'])
+            ->name('boards.create');
 
-Route::post('/boards/store', [BoardController::class, 'store'])
-    ->name('boards.store');
+        Route::post('/boards/store', [BoardController::class, 'store'])
+            ->name('boards.store');
 
-Route::get('/boards/{board}/edit', [BoardController::class, 'edit'])
-    ->name('boards.edit')
-    ->where('board', '[0-9]+');
+        Route::get('/boards/{board}/edit', [BoardController::class, 'edit'])
+            ->name('boards.edit')
+            ->where('board', '[0-9]+');
 
-Route::patch('/boards/{board}/update', [BoardController::class, 'update'])
-    ->name('boards.update')
-    ->where('board', '[0-9]+');
+        Route::patch('/boards/{board}/update', [BoardController::class, 'update'])
+            ->name('boards.update')
+            ->where('board', '[0-9]+');
 
-Route::delete('/boards/{board}/destroy', [BoardController::class, 'destroy'])
-    ->name('boards.destroy')
-    ->where('board', '[0-9]+');
+        Route::delete('/boards/{board}/destroy', [BoardController::class, 'destroy'])
+            ->name('boards.destroy')
+            ->where('board', '[0-9]+');
 
-Route::post('/boards/{board}/comments', [CommentController::class, 'store'])
-    ->name('comments.store')
-    ->where('board', '[0-9]+');
+        Route::post('/boards/{board}/comments', [CommentController::class, 'store'])
+            ->name('comments.store')
+            ->where('board', '[0-9]+');
 
-Route::delete('/comments/{comment}/destroy', [CommentController::class, 'destroy'])
-    ->name('comments.destroy')
-    ->where('comment', '[0-9]+');
+        Route::delete('/comments/{comment}/destroy', [CommentController::class, 'destroy'])
+            ->name('comments.destroy')
+            ->where('comment', '[0-9]+');
+    });
+});
 
 require __DIR__ . '/auth.php';
